7a52a7395a4dc9e6e6fa88273efd5aa4c6211a65
Anton14889 <jalssomino@gmail.com>
2020-10-23 21:02:39 +0300

    fixed canActivate

--- src/app/guards/user-form.guard.ts
+++ src/app/guards/user-form.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
@@ -11,9 +10,8 @@ export class UserFormGuard implements CanActivate {
   constructor(
     private authService: AuthService, private router: Router
   ) { }
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+  canActivate(): boolean {
     if (!this.authService.isLoggedIn()) {
       this.router.navigateByUrl('/');
     }
