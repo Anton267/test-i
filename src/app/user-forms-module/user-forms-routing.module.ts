31acdee77600af5db1fb2e7ad65828c939e10e63
Anton14889 <jalssomino@gmail.com>
2020-10-23 14:25:48 +0300

    user form guard

--- src/app/user-forms-module/user-forms-routing.module.ts
+++ src/app/user-forms-module/user-forms-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserFormGuard } from '../user-form.guard';
+import { UserFormGuard } from '../guards/user-form.guard';
 import { UserFormsPageComponent } from '../user-forms-page/user-forms-page.component';
 
 const routes: Routes = [
