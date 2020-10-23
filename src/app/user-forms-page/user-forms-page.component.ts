513bdb5591a17e9156cf59d807160208d02e9d9b
Anton14889 <jalssomino@gmail.com>
2020-10-23 21:15:41 +0300

    added logout method

--- src/app/user-forms-page/user-forms-page.component.ts
+++ src/app/user-forms-page/user-forms-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-user-forms-page',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+
+  logout(): void {
+    this.authService.logout()
+      .subscribe(success => {
+        if (success) {
+          this.router.navigateByUrl('/');
+        }
+      });
+  }
 
   ngOnInit(): void {
   }
