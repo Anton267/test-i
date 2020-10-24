729876b8ae70aa5d59b05dbbcf61d62ccabc9c62
Anton14889 <jalssomino@gmail.com>
2020-10-24 10:43:36 +0300

    added expanded panel

--- src/app/pages/user-forms-page/user-table/user-table.component.ts
+++ src/app/pages/user-forms-page/user-table/user-table.component.ts
@@ -1,5 +1,5 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -8,33 +8,26 @@ import { ApiService } from 'src/app/shared/api.service';
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.sass']
+  styleUrls: ['./user-table.component.sass'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class UserTableComponent implements OnInit {
 
-  public dynamicForm: FormGroup;
   public dataSource: MatTableDataSource<any>;
   public displayedColumns: string[] = ['id', 'title', 'type', 'updated_at'];
   @ViewChild(MatPaginator) public paginator: MatPaginator;
   @ViewChild(MatSort) public sort: MatSort;
+  public expandedElement: string;
 
   constructor(
     private api: ApiService,
-    private fb: FormBuilder,
-  ) {
-    this.dynamicForm = this.fb.group({
-      // userForm: ['', Validators.required],
-      userDynamicsForms: new FormArray([])
-    });
-  }
-
-  get controls(): { [key: string]: AbstractControl } {
-    return this.dynamicForm.controls;
-  }
-
-  get userDynamicsForms(): FormArray {
-    return this.controls.userDynamicsForms as FormArray;
-  }
+  ) { }
 
   applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -47,17 +40,9 @@ export class UserTableComponent implements OnInit {
   ngOnInit(): void {
     this.api.getFormList().subscribe(formList => {
       console.log(formList);
-      // formList.data.forEach(item => {
-      //   const form = {};
-      //   form[item.title] = ['', Validators.required];
-      //   const group = this.fb.group(form);
-      //   this.userDynamicsForms.push(group);
-      // });
-      const x = [...formList.data, ...formList.data.slice(0, 4)];
-      this.dataSource = new MatTableDataSource(x);
+      this.dataSource = new MatTableDataSource(formList.data);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-      // console.log(this.userDynamicsForms);
     });
   }
 
