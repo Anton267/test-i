909a6c13bd8096180cb141e99f816dcb5a6f7538
Anton14889 <jalssomino@gmail.com>
2020-10-24 13:08:27 +0300

    added  ngx toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TokenInterceptor } from './auth/token.interceptor';
 import { UserFormsModule } from './pages/user-forms-page/user-forms.module';
 import { LoginComponent } from './pages/login/login.component';
 import { MaterialModule } from './shared-modules/material.module';
 import { SharedModule } from './shared-modules/shared.module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -25,6 +25,7 @@ import { SharedModule } from './shared-modules/shared.module';
     UserFormsModule,
     MaterialModule,
     SharedModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     {
